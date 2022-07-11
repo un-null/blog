431d8f1f0fca33cc4e891822fa0bf06570663216
un-null <movigam.0902@gmail.com>
2022-07-11 16:34:13 +0900

    🎉

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-blue-400 text-3xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
