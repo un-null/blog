6cbbb4a73c890e30ad37cb999035cd6ecd90ecee
un-null <movigam.0902@gmail.com>
2022-07-11 17:13:44 +0900

    ✨ MicroCMSから記事一覧取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "../libs/client";
+import { Blog } from "../types";
 
-const Home: NextPage = () => {
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
   return (
     <div>
       <h1 className="font-bold text-blue-400 text-3xl">Hello World</h1>
+      <ul>
+        {props.contents.map((content) => (
+          <li key={content.id}>
+            <Link href={`/`}>
+              <a>{content.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.getList<Blog>({
+    endpoint: "blog",
+  });
+  return {
+    props: data,
+  };
+};
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type Blog = {
+  id: string;
+  title: string;
+  body: string;
+  image?: string;
+  tag?: string;
+};
